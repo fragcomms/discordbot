9e5d2d2936b761b01e3a8fc58548e0706b6f9520
default <hatomemes@gmail.com>
2025-10-19 20:24:25 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { Client, Events, GatewayIntentBits } from 'discord.js'
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-console.log("hello hi hello")
 client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.login(process.env.DISCORD_TOKEN)

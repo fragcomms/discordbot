a896855c2fd7c98e3e3726db9f462f3ed1b2fd5b
default <shannon.belton97@gmail.com>
2025-10-19 20:04:02 +0000

    hellohihello

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Client, Events, GatewayIntentBits } from 'discord.js'
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-console.log("hello hi hello")
+
 client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
 });

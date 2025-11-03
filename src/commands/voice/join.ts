b40fcd5756057b00a9e13963e6a288c64199e042
default <hatomemes@gmail.com>
2025-11-03 03:43:13 +0000

    formatting?

--- src/commands/voice/join.ts
+++ src/commands/voice/join.ts
@@ -1,34 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ChatInputCommandInteraction, SlashCommandBuilder, GuildMember, GatewayIntentBits, Client, InteractionCallback } from "discord.js";
 import { joinVoiceChannel, getVoiceConnection } from '@discordjs/voice'
 
 const data = new SlashCommandBuilder().setName('join').setDescription('Allows the bot to join the same channel as the user.');
 
 async function execute(interaction: ChatInputCommandInteraction) {
+  const member = interaction.member as GuildMember;
+  const channel = member.voice?.channel;
 
+  if (!channel) { 
+    await interaction.reply('Use this command when you are in a voice channel!')
+    return
+   }
 
-    const member = interaction.member as GuildMember;
-    const channel = member.voice?.channel;
-    const channel_id = member.voice?.channelId;
-
-    if(!channel) {return}
-    if(!channel_id) {return}
-
-    const alreadyConnected = getVoiceConnection(interaction.guildId!);
-    if (alreadyConnected) {
+  const alreadyConnected = getVoiceConnection(channel.guild.id);
+  if (alreadyConnected) {
     alreadyConnected.destroy();
   }
-    
-    joinVoiceChannel({
-        debug: true,
-        adapterCreator: channel?.guild.voiceAdapterCreator,
-        channelId: channel_id,
-        guildId: channel?.guild.id,
-        selfDeaf: false
-    
-    });
 
-    console.log(`joined ${channel.name}, id: ${channel_id}`);
+  joinVoiceChannel({
+    adapterCreator: channel.guild.voiceAdapterCreator,
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    selfDeaf: false
+  });
+
+  console.log(`joined ${channel.name}, id: ${channel.id}`);
 
 }
 
-export {data, execute}
+export { data, execute }

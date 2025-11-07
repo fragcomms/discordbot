398969aa091545445c83e0c960a168ad5f5f8010
default <hatomemes@gmail.com>
2025-11-07 09:42:08 +0000

    comments

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,3 +1,4 @@
+// test command
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 const data = new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
--- src/commands/utility/user.ts
+++ src/commands/utility/user.ts
@@ -1,3 +1,4 @@
+// test command
 import { SlashCommandBuilder, ChatInputCommandInteraction, GuildMember } from 'discord.js';
 
 const data = new SlashCommandBuilder().setName('user').setDescription('Provides information about the user.')
@@ -7,10 +8,9 @@ async function execute(interaction: ChatInputCommandInteraction) {
     await interaction.reply('Use this bot in discord servers only!')
     return
   }
-  
 
   const member: GuildMember = interaction.guild?.members.cache.get(interaction.user.id) ?? (await interaction.guild!.members.fetch(interaction.user.id))
-  await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${member.joinedAt?.toDateString()}`)
+  await interaction.reply(`This command was run by <@${interaction.user.id}>, who joined on ${member.joinedAt?.toDateString()}`)
 }
 
 export { data, execute }

23999a8559647d2672d071bf443e30a038bdf39e
default <hatomemes@gmail.com>
2025-11-07 09:38:18 +0000

    comments

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { ChatInputCommandInteraction, Client, ClientUser, Collection, Events, GatewayIntentBits, MessageFlags, GuildChannelManager, TextChannel } from 'discord.js'
-import { ExtendedClient } from './ExtendedClient.js'
+import { ExtendedClient } from './types/ExtendedClient.js'
 import { fileURLToPath } from 'node:url'
 
 const client = new ExtendedClient()
@@ -15,7 +15,7 @@ client.commands = new Collection()
 const foldersPath = path.join(__dirname, 'commands')
 const commandFolders = fs.readdirSync(foldersPath)
 
-// run for all folders to check if commands are correct
+// run for all folders to check if commands are correct - aaron
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder)
   const commandFiles = fs.readdirSync(commandsPath).filter((file) => /\.(js|ts)$/.test(file))
@@ -30,7 +30,7 @@ for (const folder of commandFolders) {
   }
 }
 
-// events support
+// events decentralization (every event can be coded independently in events folder) - aaron
 const eventsPath = path.join(__dirname, 'events')
 const eventFiles = fs.readdirSync(eventsPath) // list of things inside 'events' directory
 for (const file of eventFiles) {

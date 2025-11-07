f66d576c1ad79744d63e64733fe35e531cc27dee
default <hatomemes@gmail.com>
2025-11-07 09:39:05 +0000

    moved to types to have better definition of what types are

---
+++ src/types/ExtendedClient.ts
@@ -0,0 +1,19 @@
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import type { Command } from './Command.js'
+
+// discordjs doesn't have "commands", typescript requires that
+// this extendedclient exist purely because of "commands" - aaron
+class ExtendedClient extends Client {
+	commands: Collection<string, Command>
+
+	constructor() {
+		super({ intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildVoiceStates ] });
+    this.commands = new Collection
+	}
+}
+
+export { ExtendedClient }

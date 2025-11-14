7cf31352409d97617d93a347be656c853f9a28f2
default <shannon.belton97@gmail.com>
2025-11-14 20:52:25 +0000

    logic moved to cleanUpProcess

--- src/commands/voice/stop-recording.ts
+++ src/commands/voice/stop-recording.ts
@@ -1,11 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
-import fs from "fs";
-import { exec } from "child_process";
-import { recordings, Recording } from '../utility/recordings.js';
-import { convertMultiplePcmToMka, convertPcmToWav } from '../utility/audio-conversion.js';
-import ffmpeg from 'ffmpeg-static'
-import path from "path";
+import { cleanUpProcess } from "../../events/disconnect.js";
+import { recordings} from '../utility/recordings.js';
+
 
 const data = new SlashCommandBuilder()
   .setName('stop-recording')
@@ -23,7 +20,12 @@ async function execute(interaction: ChatInputCommandInteraction) {
   //TODO: check if user is in a channel that is currently recording
   if (!interaction.member.voice.channel) {
     await interaction.reply('Join the specific voice channel to stop recording!')
+    return;
   }
+  const channelId = interaction.channelId;
+  const client = interaction.client;
+
+
 
   if (!guildRecordings || guildRecordings.length == 0) {
     await interaction.reply('Not currently recording.');
@@ -33,29 +35,9 @@ async function execute(interaction: ChatInputCommandInteraction) {
   await interaction.reply('Stopping recording, processing files...');
 
   //STOP AND PROCESS ALL ACTIVE RECORDINGS
-  for (const recording of guildRecordings) {       // iterate through all active recordings
-    try {
-      recording.opusStream.destroy();     // stop the stream
-      const wavPath = await convertPcmToWav(recording.user, recording.filePath); // convert file, get path string
-      await interaction.followUp({
-        content: `Finished processing recording session in ${interaction.member.voice.channel?.name}. \nSaved as .wav file`,
-        files: [wavPath]
-      }
-      )
-    }
-    catch (error) {
-      console.error(error)
-      await interaction.followUp(`Could not stop recording for ${recording.user.username}:`);
-    }
-  }
-  const wavPath = await convertMultiplePcmToMka(path.join(process.cwd(), 'data', guildId), guildRecordings[0].timestamp)
-  await interaction.followUp({
-    content: `Compiled all user's recordings to one.`,
-    files: [wavPath]
-  })
+  cleanUpProcess(guildId, channelId, client);
 
-  recordings.delete(guildId) // delete once finished, we don't need to keep old streams
-}
+ }
 
 export { data, execute };
 

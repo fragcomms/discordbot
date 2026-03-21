a06d111c2710e0c18bfbd2bfa61e18d557422731
default <shannon.belton97@gmail.com>
2026-03-20 23:42:56 -0700

    fixed bug  where the bot would break whenever a user who is not in a voice channel tried to disconnect the bot

--- src/events/disconnect.ts
+++ src/events/disconnect.ts
@@ -1,28 +1,67 @@
 import { Client, Events, Guild, InteractionCallback, VoiceState } from "discord.js";
 import {  cleanUpProcess } from "../commands/utility/cleanup.js";
 import { getGuildState, setGuildState } from "../commands/utility/last-channel-interaction.js";
+import { sendMessage } from "../commands/utility/messages.js";
+import {Recording, recordings} from "../commands/utility/recordings.js"
+import { getVoiceConnection} from "@discordjs/voice";
 
 const name = Events.VoiceStateUpdate;
 async function execute(oldState: VoiceState, newState: VoiceState, client: Client) {
-  if (oldState.channel && !newState.channel) { //
 
-    const guild = oldState.guild;
-    const guildId = guild.id;
+  
+
+  // get guild id
+  const guild = oldState.guild;
+  const guildId = guild.id;
+  
+  // get guild recordings by guildId
+  const guildRecordings = recordings.get(guildId);
+  
+  
+  // find last interactions by guildId
+  const state = getGuildState(guildId)
+
+  // get necessary values for cleanup process from GuildState Map
+  const textChannelId = state?.lastTextChannelId;
+  const voiceChannelId = state?.lastVoiceChannelId;
+  const sessionClient = state?.client;
+
+  if (oldState.channel && !newState.channel || oldState.channel && newState.channel) { // handle disconnects & vc switches
+
+
+    const vcMembers = oldState.channel.members.filter(member => !member.user.bot); // all human members in vc
+    const theBot = oldState.channel.members.filter(member => member.user.bot) // the bot's status in the vc
+    const theBotIsAlone = (vcMembers.size > 0 && theBot.size === 1);
+    const connection = getVoiceConnection(guildId);
+
+    if (!oldState.channel) return; 
+    if (!oldState.channel.members) return; 
+    if (oldState.member?.id === sessionClient!.user?.id) {
+      connection?.destroy();
+      return;
+    }
+
     
-    const state = getGuildState(guildId)
 
-    const textChannelId = state?.lastTextChannelId;
-    const voiceChannelId = state?.lastVoiceChannelId;
-    const client = state?.client;
+    if(theBotIsAlone) {
+      connection?.destroy();
+    }
 
-    // console.log(`CHANNEL ID = ${channelId}`);
-    cleanUpProcess(
-      guildId!, 
-      textChannelId!,
-      voiceChannelId!, 
-      client!);
-    // cleanUpDirectory("data");
-  }
+    if (guildRecordings && guildRecordings.length > 0 && (vcMembers.size === 0 || theBot.size === 0)) { // if recording in progress, but either the bot leaves or everybody but the bot leaves
+      // console.log(`CHANNEL ID = ${channelId}`);
+      cleanUpProcess(         // wrap it up
+        guildId!, 
+        textChannelId!,
+        voiceChannelId!, 
+        sessionClient!
+      );
+      connection?.destroy();
+      
+    }
+
+  } 
+
+
 }
 
 export { execute, name };
